569ba85a1afeb31c9ab61f36ac0a2ac790a7612e
Sergey <xxsmotur@gmail.com>
2020-04-10 23:08:05 +0500

    Add controllers

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from "express";
+import * as homeController from "./controllers/home";
 
 const app = express();
+app.set("port", 3000);
+
+app.get("/", homeController.index);
 
 export default app;
